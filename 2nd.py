9669ca12c5684b94b66d89500c01a953541d97bb
Arshad <arshadansari8988@gmail.com>
2026-05-11 22:49:54 +0530

    add second python file

---
+++ 2nd.py
@@ -0,0 +1,25 @@
+'''TODAY I LEARNED ABOUT THE PRINT FUNCTION IN PYTHON AND ALSO I LEARNED ABOUT THE COMMENTING IN PYTHON AND ALSO I LEARNED ABOUT THE MULTI LINE COMMENT IN PYTHON AND ALSO I LEARNED ABOUT THE SEPARATING THE VALUES IN THE PRINT FUNCTION AND ALSO I LEARNED ABOUT THE ENDING THE LINE IN THE PRINT FUNCTION'''
+
+
+
+
+print ("hey i am a good boy\n and this viewer is also a good man") # this is the code for printing the string in the output and also we can use \n for new line in the output
+#author :Arshad Ansari
+
+#print("hello world ") # this is the  code for commenting the line and it will not be executed in the output
+
+print("hello world \"how are you\"")
+
+
+
+
+#multi line comment triple single quotes
+
+''' this is a multi line comment and it will not be executed in the output and we can use three single quotes for multi line comment and also we can use three double quotes for multi line comment'''
+
+
+
+print("hey", 6 ,7 ,sep="~",end="007\n") # this is the code for printing the string and also we can use sep for separating the values and end for ending the line in the output
+
+print("Arshad")
+
